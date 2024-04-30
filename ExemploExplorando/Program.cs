c431d44ab5b936cca804b5ad4cff0daf900061f7
Arthur Duarte <88559494+arthurddduarte86@users.noreply.github.com>
2024-04-30 07:51:44 -0300

    new

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,6 +1,43 @@
 ﻿using ExemploExplorando.Models;
+using Newtonsoft.Json;
 
 
+
+/* 
+// serialização JSON
+
+DateTime dataAtual = DateTime.Now;
+
+List<Venda> listaVendas = new List<Venda>();
+
+Venda v1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Licença de software", 100.00M, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+Console.WriteLine(serializado);
+
+File.WriteAllText("Arquivos/vendas.json", serializado);
+ */
+
+
+// des-serialização
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+foreach(Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda}");
+}
+
+
+
+
+
+/* 
 
 LeituraArquivo arquivo = new LeituraArquivo();
 
@@ -20,7 +57,7 @@ else
 }
 
 
-
+ */
 
 
 
